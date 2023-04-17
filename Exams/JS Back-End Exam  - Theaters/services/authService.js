c082704149cea052f0d6be0b02c6d3d06f32cdf5
Dimitar Mitev <d.mitev920123@gmail.com>
2023-04-17 17:43:48 +0300

    Create authService.js

---
+++ Exams/JS Back-End Exam  - Theaters/services/authService.js
@@ -0,0 +1,62 @@
+const User = require('../models/User.js');
+const bcrypt = require('bcrypt');
+
+const jwt = require('../lib/jsonwebtoken.js');
+const { SECRET } = require('../constants.js');
+
+exports.findByUsername = (username) => User.findOne({ username });
+
+exports.findById = (userId) => User.findById(userId);
+
+exports.findByIdDetailer = (userId) => User.findById(userId).populate('likedPlays');
+
+exports.register = async (username, password, repeatPassword) => {
+    if (password.length < 4) {
+        throw new Error('Password is too short!');
+    }
+
+    const regex = new RegExp('^[A-Za-z0-9]*$');
+
+    if (!regex.test(password)) {
+        throw new Error('Password must includes only english letter and digits!');
+    }
+
+
+    if (password !== repeatPassword) {
+        throw new Error('Password mismatch!');
+    }
+
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+        throw new Error('User exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({ username, password: hashedPassword });
+
+    return this.login(username, password);
+};
+
+exports.login = async (username, password) => {
+    const user = await this.findByUsername(username);
+
+    if (!user) {
+        throw new Error('Invalid username or password!');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Invalid username or password');
+    }
+
+    const payload = {
+        _id: user._id,
+        username: username
+    };
+
+    const token = await jwt.sing(payload, SECRET);
+    return token;
+};
